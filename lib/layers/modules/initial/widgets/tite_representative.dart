ecc14f3434cf9b539d1d5387470043ebf1a4e30b
Andeson Silva <andsilva607@gmail.com>
2023-04-07 21:58:10 -0300

    FE1.2 - Adicionando icone de edição

--- lib/layers/modules/initial/widgets/tite_representative.dart
+++ lib/layers/modules/initial/widgets/tite_representative.dart
@@ -43,75 +43,85 @@ class TileRepresentativeWidget extends StatelessWidget {
                 borderRadius: BorderRadius.circular(8),
               ),
               elevation: 0,
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 220,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          width: 150,
-                          child: AutoSizeText(
-                            representante.name,
-                            maxLines: 1,
-                            maxFontSize: 14,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              overflow: TextOverflow.ellipsis,
-                              color: Color(
-                                0xFF4A6F91,
-                              ),
-                              fontFamily: 'Segoe UI',
-                            ),
-                          ),
-                        ),
                         Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 2, right: 2),
-                              child: Icon(
-                                Icons.verified_outlined,
-                                size: 10,
-                                color: Color(0xFF0085FF),
-                              ),
-                            ),
                             SizedBox(
-                              width: 70,
-                              child: Text(
-                                representante.area,
+                              width: 115,
+                              child: AutoSizeText(
+                                representante.name,
+                                maxLines: 1,
+                                maxFontSize: 14,
                                 style: const TextStyle(
-                                  fontSize: 10,
-                                  color: Color(0xFF0085FF),
-                                  fontFamily: 'Segoe UI',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
                                   overflow: TextOverflow.ellipsis,
+                                  color: Color(
+                                    0xFF4A6F91,
+                                  ),
+                                  fontFamily: 'Segoe UI',
                                 ),
                               ),
                             ),
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 2, right: 2),
+                                  child: Icon(
+                                    Icons.verified_outlined,
+                                    size: 10,
+                                    color: Color(0xFF0085FF),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 90,
+                                  child: Text(
+                                    representante.area,
+                                    style: const TextStyle(
+                                      fontSize: 10,
+                                      color: Color(0xFF0085FF),
+                                      fontFamily: 'Segoe UI',
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 0.0, bottom: 1),
+                          child: SubtitleWidget(
+                            text: representante.email,
+                            icon: Icons.email_outlined,
+                          ),
+                        ),
+                        SubtitleWidget(
+                          text: representante.turn,
+                          icon: Icons.alarm,
+                        )
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 0.0, bottom: 1),
-                      child: SubtitleWidget(
-                        text: representante.email,
-                        icon: Icons.email_outlined,
-                      ),
-                    ),
-                    SubtitleWidget(
-                      text: representante.turn,
-                      icon: Icons.alarm,
-                    )
-                  ],
-                ),
+                  ),
+                  Expanded(
+                    child: Icon(Icons.edit_outlined),
+                  )
+                ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
